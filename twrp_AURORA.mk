ba8a8e1c85e0a9b492babec13cde092c758e76d3
TomKing062 <yx42_44@126.com>
2023-03-11 09:55:44 +0800

    Update twrp_AURORA.mk

--- twrp_AURORA.mk
+++ twrp_AURORA.mk
@@ -12,16 +12,34 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from AURORA device
-$(call inherit-product, device/chinatelecom/AURORA/device.mk)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
 
 PRODUCT_DEVICE := AURORA
 PRODUCT_NAME := twrp_AURORA
 PRODUCT_BRAND := Tianyi1Hao2021
 PRODUCT_MODEL := TYH211U
 PRODUCT_MANUFACTURER := chinatelecom
-
-PRODUCT_GMS_CLIENTID_BASE := android-chinatelecom
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ud710_7h10_ctcc-user 10 QP1A.190711.020 47316 release-keys"
