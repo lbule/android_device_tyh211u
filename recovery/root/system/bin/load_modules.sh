c9c16e8fc3e125bb5cd20f0561e29ce06d92c6bb
TomKing062 <yx42_44@126.com>
2023-03-11 16:48:41 +0800

    Create load_modules.sh

---
+++ recovery/root/system/bin/load_modules.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+load()
+{
+        mkdir /v
+        mount -t ext4 -o ro /dev/block/mmcblk0p42 /v
+        mkdir -p /vendor/lib/modules
+        cp /v/synaptics_dsx_td4310.ko /vendor/lib/modules/synaptics_dsx_td4310.ko
+        cp /v/synaptics_td4320_spi_ts.ko /vendor/lib/modules/synaptics_td4320_spi_ts.ko
+        insmod /vendor/lib/modules/synaptics_dsx_td4310.ko
+        insmod /vendor/lib/modules/synaptics_td4320_spi_ts.ko
+        umount /v
+        rmdir /v
+}
+
+load
+wait 1
+
+setprop modules.loaded 1
+
+exit 0
